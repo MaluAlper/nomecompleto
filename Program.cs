3aef5e803748c461cbd2c7409ba0829631f3451e
Maria Luiza <malualper@hotmail.com>
2020-05-15 21:15:56 -0300

    nomecompleto

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace nomecompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine ("Digite seu primeiro nome");
+            string nome = Console.ReadLine();
+
+             Console.WriteLine ("digite seu Segundo nome");
+            string segundonome = Console.ReadLine();
+            
+            Console.WriteLine ("Seu nome completo é "+ nome + " " + segundonome);
+
+            Console.WriteLine ("Seu nome catalágo é " + segundonome.ToUpper() + " " + nome);
+            
+            
+
+           
+
+
+
+
+        }   
+    }
+}
